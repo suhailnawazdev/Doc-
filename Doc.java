fe3a69edeb7cc8d9aacf04e6d233c18e530c8cbe
suhailnawazdev <109364334+suhailnawazdev@users.noreply.github.com>
2022-07-15 21:11:04 +0530

    Add files via upload

---
+++ Doc.java
@@ -0,0 +1,24 @@
+import java .util.Scanner;
+
+class Doc
+
+{
+
+public static void main(String[] args)
+
+{
+
+Double r, doc;
+
+System.out.println("Enter the diameter");
+
+Scanner s = new Scanner (System.in);
+
+r= s.nextDouble();
+
+doc=r*2;
+
+System.out.println("diameter is"+ doc);
+
+}
+}
